235e083af063f49edf59dcd7b43c5af0b330a8ef
rotate720deg <395340017@qq.com>
2017-09-30 18:02:42 +0800

    功能设计-添加目录结构

---
+++ middlewares/check.js
@@ -0,0 +1,9 @@
+module.exports = {
+	checkLogin: function checkLogin(req,res,next){
+		if(!req.session.user){
+			req.flash({'error':'未登录'});
+			return res.redirect('/signin');
+		}
+		next();
+	},
+}
